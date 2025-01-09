StructuralEquationModels/StructuralEquationModels.jl

declare cov matrices symmetric

src/frontend/fit/fitmeasures/minus2ll.jl
@@ -49,7 +49,7 @@ function minus2ll(observed::SemObservedMissing)
     for pat in observed.patterns
         nᵢ = nsamples(pat)
         # implied covariance/mean
-        Σᵢ = Σ[pat.obs_mask, pat.obs_mask]
+        Σᵢ = Symmetric(Σ[pat.obs_mask, pat.obs_mask])
 
         ld = logdet(Σᵢ)
         Σᵢ⁻¹ = LinearAlgebra.inv!(cholesky!(Σᵢ))

sem-jl/SEM.jl

src/frontend/fit/fitmeasures/minus2ll.jl
@@ -50,7 +50,7 @@ function minus2ll(observed::SemObservedMissing)
     # FIXME: this code is duplicate to objective(fiml, ...)
     F = sum(observed.patterns) do pat
         # implied covariance/mean
-        Σᵢ = Σ[pat.measured_mask, pat.measured_mask]
+        Σᵢ = Symmetric(Σ[pat.measured_mask, pat.measured_mask])
         Σᵢ_chol = cholesky!(Σᵢ)
         ld = logdet(Σᵢ_chol)
         Σᵢ⁻¹ = LinearAlgebra.inv!(Σᵢ_chol)